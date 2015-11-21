27bcc8edc5e116d5b31342585e76b9e6eed290f8
naveen <naveen.kumar4@cognizant.com>
2015-11-21 00:44:38 +0000

    first commit

---
+++ build/img/jquery.parmovement.js
@@ -0,0 +1,95 @@
+ 
+(function ($) {
+	$.fn.parmovement = $.fn.parmovement || function(options) {
+		//	What selector to use when finding scrollable elements
+		options = $.extend({ scrollable: '.scrollable' }, options);
+
+		 
+		var ua = navigator.userAgent,
+			 
+			getTransform = function() {
+				var prefixes = 'transform WebkitTransform MozTransform OTransform msTransform'.split(' ');
+				for(var i = 0; i < prefixes.length; i++) {
+					if(document.createElement('div').style[prefixes[i]] !== undefined) {
+						return prefixes[i];
+					}
+				}
+				return false;
+			},
+			transform = getTransform(),
+			 
+			force2d = ua.match(/msie 9/i),
+			run = !(ua.match(/msie 6/i) || ua.match(/mobile/i));
+			scrollItems = [],
+			scrollHandler = null,
+			
+			//	Create a scrollable
+			Scrollable = function($el) {
+				// set up some vars for caching values throughout
+				$el = $($el);
+					
+				var $para = $el.find(options.scrollable),
+					elTop = $el.offset().top,
+					maxOffset = [],
+					height = $el.height();
+
+				// the actual parallax moving function
+				function refresh(docScrollY) {
+					$para.each(function (idx, $pEl) {
+						var offset, translate;
+						$pEl = $($pEl);
+						// offset is the document scroll minus the top of the current $el
+						// divided by the $el's height multiplied by the maximum offset.
+						offset = -Math.round(((docScrollY - elTop) / height) * maxOffset[idx]);
+						offset = Math.max(offset, maxOffset[idx]);
+
+						translate = force2d ?
+							'translateY(' + offset + 'px)':
+							'translate3d(0px,' + offset + 'px, 0px)';
+
+						if (transform && translate) {
+							$pEl.css(transform, translate);
+							$pEl.css('transform', translate);
+						} else {
+							$pEl.css('marginTop', offset + 'px');
+						}
+					});
+				};
+
+				//	Maximum offset is the height of the element minus the height of the parallax background + 2px
+				$para.each(function (idx, item) {
+					maxOffset.push(height - $(item).height() + 2);
+				});
+
+				//	Expose refresh
+				return { 'refresh': refresh };
+			},
+
+			//	Update scrollable items
+			updateScrollable = function() {
+				var docScrollY = $(window).scrollTop();
+				$.each(scrollItems, function(idx, item){
+					item.refresh(docScrollY);
+				})
+			},
+
+			//	Add a parralax item
+			addParallax = function(node) {
+				scrollItems.push(new Scrollable(node));
+				if (!scrollHandler) {
+					scrollHandler = true;
+					$( window ).scroll(updateScrollable);
+				}
+			};
+
+		if (run) {
+			//  Find all parallax items
+			this.each(function (idx, item) {
+				addParallax(item);
+			});
+
+			// Load in the coordinates of the parallax pieces when the document loads
+			updateScrollable();
+		}
+	};
+}(window.jQuery));
---
+++ server.js
@@ -0,0 +1,182 @@
+/**
+ * OAuth
+ */
+
+var path = require('path');
+var qs = require('querystring');
+
+
+var bodyParser = require('body-parser');
+var cors = require('cors');
+var express = require('express');
+var logger = require('morgan');
+var request = require('request');
+
+
+
+var app = express();
+
+app.set('port', process.env.PORT || 3000);
+app.use(cors());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Force HTTPS on Heroku
+if (app.get('env') === 'production') {
+  app.use(function(req, res, next) {
+    var protocol = req.get('x-forwarded-proto');
+    protocol == 'https' ? next() : res.redirect('https://' + req.hostname + req.url);
+  });
+}
+app.use(express.static(path.join(__dirname, './build')));
+
+var profileDate;
+app.get('/api/me', function(req, res) {
+  res.send(profileDate);
+});
+
+/*
+ |--------------------------------------------------------------------------
+ | Login with Facebook
+ |--------------------------------------------------------------------------
+ */
+app.post('/auth/facebook', function(req, res) {
+  var fields = ['id', 'email', 'first_name', 'last_name', 'link', 'name'];
+  var accessTokenUrl = 'https://graph.facebook.com/v2.5/oauth/access_token';
+  var graphApiUrl = 'https://graph.facebook.com/v2.5/me?fields=' + fields.join(',');
+  var params = {
+    code: req.body.code,
+    client_id: req.body.clientId,
+    client_secret: config.FACEBOOK_SECRET,
+    redirect_uri: req.body.redirectUri
+  };
+
+  // Step 1. Exchange authorization code for access token.
+  request.get({ url: accessTokenUrl, qs: params, json: true }, function(err, response, accessToken) {
+    if (response.statusCode !== 200) {
+      return res.status(500).send({ message: accessToken.error.message });
+    }
+
+    // Step 2. Retrieve profile information about the current user.
+    request.get({ url: graphApiUrl, qs: accessToken, json: true }, function(err, response, profile) {
+      if (response.statusCode !== 200) {
+        return res.status(500).send({ message: profile.error.message });
+      }
+      console.log(profile);
+      profileDate=profile;
+      res.send({ token: profile });
+
+
+    
+    });
+  });
+});
+
+/*
+ |--------------------------------------------------------------------------
+ | Login with LinkedIn
+ |--------------------------------------------------------------------------
+ */
+app.post('/auth/linkedin', function(req, res) {
+  var accessTokenUrl = 'https://www.linkedin.com/uas/oauth2/accessToken';
+  var peopleApiUrl = 'https://api.linkedin.com/v1/people/~:(id,first-name,last-name,email-address,picture-url)';
+  var params = {
+    code: req.body.code,
+    client_id: req.body.clientId,
+    client_secret: config.LINKEDIN_SECRET,
+    redirect_uri: req.body.redirectUri,
+    grant_type: 'authorization_code'
+  };
+
+  // Step 1. Exchange authorization code for access token.
+  request.post(accessTokenUrl, { form: params, json: true }, function(err, response, body) {
+    if (response.statusCode !== 200) {
+      return res.status(response.statusCode).send({ message: body.error_description });
+    }
+    var params = {
+      oauth2_access_token: body.access_token,
+      format: 'json'
+    };
+
+    // Step 2. Retrieve profile information about the current user.
+    request.get({ url: peopleApiUrl, qs: params, json: true }, function(err, response, profile) {
+      profileDate=profile;
+      res.send({ token: (profile) });
+      
+    });
+  });
+});
+
+
+
+
+/*
+ |--------------------------------------------------------------------------
+ | Login with Twitter
+ |--------------------------------------------------------------------------
+ */
+app.post('/auth/twitter', function(req, res) {
+  var requestTokenUrl = 'https://api.twitter.com/oauth/request_token';
+  var accessTokenUrl = 'https://api.twitter.com/oauth/access_token';
+  var profileUrl = 'https://api.twitter.com/1.1/users/show.json?screen_name=';
+
+  // Part 1 of 2: Initial request from Satellizer.
+  if (!req.body.oauth_token || !req.body.oauth_verifier) {
+    var requestTokenOauth = {
+      consumer_key: config.TWITTER_KEY,
+      consumer_secret: config.TWITTER_SECRET,
+      callback: req.body.redirectUri
+    };
+
+    // Step 1. Obtain request token for the authorization popup.
+    request.post({ url: requestTokenUrl, oauth: requestTokenOauth }, function(err, response, body) {
+      var oauthToken = qs.parse(body);
+
+      // Step 2. Send OAuth token back to open the authorization screen.
+      res.send(oauthToken);
+    });
+  } else {
+    // Part 2 of 2: Second request after Authorize app is clicked.
+    var accessTokenOauth = {
+      consumer_key: config.TWITTER_KEY,
+      consumer_secret: config.TWITTER_SECRET,
+      token: req.body.oauth_token,
+      verifier: req.body.oauth_verifier
+    };
+
+    // Step 3. Exchange oauth token and oauth verifier for access token.
+    request.post({ url: accessTokenUrl, oauth: accessTokenOauth }, function(err, response, accessToken) {
+
+      accessToken = qs.parse(accessToken);
+
+      var profileOauth = {
+        consumer_key: config.TWITTER_KEY,
+        consumer_secret: config.TWITTER_SECRET,
+        oauth_token: accessToken.oauth_token
+      };
+
+      // Step 4. Retrieve profile information about the current user.
+      request.get({
+        url: profileUrl + accessToken.screen_name,
+        oauth: profileOauth,
+        json: true
+      }, function(err, response, profile) {
+        profileDate=profile;
+        res.send({ token: (profile) });
+        // Step 5a. Link user accounts.
+        
+      });
+    });
+  }
+});
+
+
+/*
+ |--------------------------------------------------------------------------
+ | Start the Server
+ |--------------------------------------------------------------------------
+ */
+app.listen(app.get('port'), function() {
+  console.log('Express server listening on port ' + app.get('port'));
+});
